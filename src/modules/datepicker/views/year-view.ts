e88730e1ed0060512c067c18f8ccbc869a06c0e6
Ilya Karpuk <ilya@topnlab.ru>
2021-05-06 16:53:07 +0300

    Fix year

--- src/modules/datepicker/views/year-view.ts
+++ src/modules/datepicker/views/year-view.ts
@@ -19,7 +19,7 @@ export class CalendarRangeYearService extends CalendarRangeService {
     <tr>
         <th colspan="3">
             <sui-calendar-view-title [ranges]="ranges" (zoomOut)="zoomOut()">
-                {{ pad(decadeStart) }} - {{ pad(decadeStart + 10) }}
+                {{ pad(decadeStart) }} - {{ pad(decadeStart + 9) }}
             </sui-calendar-view-title>
         </th>
     </tr>
